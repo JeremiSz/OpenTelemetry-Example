2bc5954c84ed1940d84da04bb433dcd4c0541875
Liam <Liam.Hurley2@students.ittralee.ie>
2022-01-26 08:32:53 +0000

    F0001 - Cleaned up activity tags and made them more descriptive. Renamed endpoints.

--- src/M1/Program.cs
+++ src/M1/Program.cs
@@ -1,12 +1,12 @@
 using System.Diagnostics;
-
 using OpenTelemetry;
 using OpenTelemetry.Trace;
 using OpenTelemetry.Resources;
 
-// Define some important constants and the activity source
 var serviceName = "CCS.OpenTelemetry.M1";
 var serviceVersion = "1.0.0";
+
+// https://github.com/open-telemetry/opentelemetry-dotnet/blob/main/src/OpenTelemetry.Exporter.Jaeger/README.md
 
 using var tracerProvider = Sdk.CreateTracerProviderBuilder()
     .AddSource(serviceName)
@@ -17,29 +17,26 @@ using var tracerProvider = Sdk.CreateTracerProviderBuilder()
     .Build();
 
 var MyActivitySource = new ActivitySource(serviceName);
+using var activity = MyActivitySource.StartActivity("M1");
 
-using var activity = MyActivitySource.StartActivity("SayHello");
-activity?.SetTag("universal?", "this is universal");
-
-
-
-// https://github.com/open-telemetry/opentelemetry-dotnet/blob/main/src/OpenTelemetry.Exporter.Jaeger/README.md
-
-
+// Can set universal tags that get sent with all requests made to the API
+// activity?.SetTag("universal?", "this is universal");
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => {
-    activity?.SetTag("foo", 1);
-    activity?.SetTag("req", "This was a get");
-    activity?.SetTag("baz", new int[] { 1, 2, 3 });
-});
+app.MapGet("/api/v1/student", () =>
+{
+    activity?.SetTag("Get", "Get request made to M1.");
+})
+    .Produces(StatusCodes.Status200OK)
+    .Produces(StatusCodes.Status404NotFound);
 
-app.MapPost("/", () => {
-    activity?.SetTag("foo", 1);
-    activity?.SetTag("req", "This was a post");
-    activity?.SetTag("baz", new int[] { 1, 2, 3 });
-});
+app.MapPost("/api/v1/student", () =>
+{
+    activity?.SetTag("Post", "Post request made to M1");
+})
+    .Produces(StatusCodes.Status200OK)
+    .Produces(StatusCodes.Status400BadRequest);
 
 app.Run();
