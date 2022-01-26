39da4d7ca41443c283c1a81cbace1e821e153692
Jeremi Sz <jeremi.szlapka@students.ittralee.ie>
2022-01-26 09:52:21 +0000

    F0002 - Created basic API for M2

---
+++ src/M2/Controllers/StudentController.fs
@@ -0,0 +1,17 @@
+﻿namespace M2.Controllers
+
+open System
+open System.Collections.Generic
+open System.Linq
+open System.Threading.Tasks
+open Microsoft.AspNetCore.Mvc
+open Microsoft.Extensions.Logging
+
+[<ApiController>]
+[<Route("backend")>]
+type StudentController (logger : ILogger<StudentController>) =
+    inherit ControllerBase()
+
+    [<HttpGet>]
+    member _.Get() =
+        "Joe Doe"
