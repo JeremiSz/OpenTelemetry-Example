6f099f84c7298aa924565f40aee626202f376d1f
Liam <Liam.Hurley2@students.ittralee.ie>
2022-01-27 09:17:32 +0000

    F0001 - M1 fixed. Traces can now be viewed live.

--- src/M1/Program.cs
+++ src/M1/Program.cs
@@ -6,41 +6,42 @@ using OpenTelemetry.Resources;
 var serviceName = "CCS.OpenTelemetry.M1";
 var serviceVersion = "1.0.0";
 
-// https://github.com/open-telemetry/opentelemetry-dotnet/blob/main/src/OpenTelemetry.Exporter.Jaeger/README.md
+var builder = WebApplication.CreateBuilder(args);
 
-using var tracerProvider = Sdk.CreateTracerProviderBuilder()
+builder.Services.AddOpenTelemetryTracing(b =>
+{
+    b
+    .AddJaegerExporter()
     .AddSource(serviceName)
     .SetResourceBuilder(
         ResourceBuilder.CreateDefault()
             .AddService(serviceName: serviceName, serviceVersion: serviceVersion))
-    .AddJaegerExporter()
-    .Build();
-
+    .AddHttpClientInstrumentation()
+    .AddAspNetCoreInstrumentation();
+});
 var MyActivitySource = new ActivitySource(serviceName);
-using var activity = MyActivitySource.StartActivity("M1");
 
-// Can set universal tags that get sent with all requests made to the API
-// activity?.SetTag("universal?", "this is universal");
 
-var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/api/v1/student", () =>
 {
+    using var activity = MyActivitySource.StartActivity("M1"); 
     activity?.SetTag("Get", "Get request made to M1.");
-    HttpClient client = new HttpClient();
-    HttpRequestMessage httpRequest = new HttpRequestMessage(HttpMethod.Get, "https://localhost:7225/backend");
-    HttpResponseMessage httpResponse = client.Send(httpRequest);
-    return httpResponse.ToString();
-});
-//.Produces(StatusCodes.Status200OK)
-//.Produces(StatusCodes.Status404NotFound);
+    //HttpClient client = new HttpClient();
+    //HttpRequestMessage httpRequest = new HttpRequestMessage(HttpMethod.Get, "https://localhost:7225/backend");
+    //HttpResponseMessage httpResponse = client.Send(httpRequest);
+    return "Get Students";
+})
+    .Produces(StatusCodes.Status200OK)
+    .Produces(StatusCodes.Status404NotFound);
 
 app.MapPost("/api/v1/student", () =>
 {
+    using var activity = MyActivitySource.StartActivity("M1"); 
     activity?.SetTag("Post", "Post request made to M1");
-});
-    //.Produces(StatusCodes.Status200OK)
-    //.Produces(StatusCodes.Status400BadRequest);
+})
+    .Produces(StatusCodes.Status200OK)
+    .Produces(StatusCodes.Status400BadRequest);
 
 app.Run();
