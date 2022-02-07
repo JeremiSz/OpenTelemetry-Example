823b37d6e02f403f50ba6f90a97a176e9e9aac9e
Liam <Liam.Hurley2@students.ittralee.ie>
2022-02-07 15:47:27 +0000

    F0001 - Testing Prometheus config.

--- src/M1/Controllers/StudentController.cs
+++ src/M1/Controllers/StudentController.cs
@@ -11,6 +11,7 @@ using OpenTelemetry.Resources;
 using System.Diagnostics;
 using System.Diagnostics.Metrics;
 
+
 namespace M1.Controllers
 {
    [ApiController]
@@ -18,13 +19,19 @@ namespace M1.Controllers
     public class StudentController : Controller
     {
         Meter meter = new Meter("M1 Controller");
-        
+        Counter<long> counter;
+        Histogram<double> histogram;
+        Random random = new Random();
+
         public StudentController(ActivitySource activitySource)
         {
             if (activitySource == null)
                 throw new ArgumentNullException(nameof(activitySource));
 
             this.activitySource = activitySource;
+
+            counter = meter.CreateCounter<long>("RequestCounter", "total");
+            histogram = meter.CreateHistogram<double>("RandomHistogram");
         }
 
         ActivitySource activitySource;
@@ -34,6 +41,8 @@ namespace M1.Controllers
         {
             var activity = Activity.Current;
             activity?.SetTag("Get", "Get request made to M1.");
+
+            counter.Add(1);
 
             helper();
             
@@ -50,12 +59,15 @@ namespace M1.Controllers
             var activity = Activity.Current;
             activity?.SetTag("Post", "Post request made to M1.");
 
-            helper();
-            
             HttpClient client = new HttpClient();
             HttpRequestMessage httpRequest = new HttpRequestMessage(HttpMethod.Post, "https://localhost:7225/backend");
-            HttpResponseMessage httpResponse = client.Send(httpRequest); 
-            
+            HttpResponseMessage httpResponse = client.Send(httpRequest);
+
+#pragma warning disable CS8620 // Argument cannot be used for parameter due to differences in the nullability of reference types.
+            histogram.Record(random.Next(10), KeyValuePair.Create<string, object>("hello", "hi"));
+#pragma warning restore CS8620 // Argument cannot be used for parameter due to differences in the nullability of reference types.
+
+
             return "post students";
         }
 
@@ -63,6 +75,7 @@ namespace M1.Controllers
         {
             using var activity = activitySource.StartActivity("M1");
             activity?.SetBaggage("Done", "helper");
+            
         }
     }
 }
